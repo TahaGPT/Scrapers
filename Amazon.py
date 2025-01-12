5a49cc258b5a7c92428c6ecd70b8b468c13b3515
TahaGPT <tahaaahmed12115@gmail.com>
2025-01-12 21:39:06 +0500

    503 error coped

--- Amazon.py
+++ Amazon.py
@@ -6,8 +6,21 @@ from selenium import webdriver
 import requests
 from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.edge.options import Options as EdgeOptions
+# from fake_useragent import UserAgent
 
+headers = {"User-Agent": 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Mobile Safari/537.36'}
 
+def generateMenu(url):
+    # session = requests.Session()
+    # session.headers.update({"User-Agent": UserAgent().random})
+    response = req.get(url, headers  = headers)
+    if not response.ok:
+        print("Server responded with exit code:", response.status_code) # if scrapping is not allowed
+        return None
+    else:    
+        soup = bsp(response.text, 'lxml')
+        links = soup.find_all('div', class_ = 'a-link-normal _fluid-quad-image-label-v2_style_centerImage__30wh- aok-block image-window')
+        
 
 
 if __name__ == '__main__':
@@ -27,5 +40,6 @@ if __name__ == '__main__':
         'Parent_asin' : [],
         'Bought_Together' : [] 
     }
+    generateMenu('https://www.amazon.com/')
 
     
